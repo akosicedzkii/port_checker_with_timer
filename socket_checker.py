ac44260bce23c179be29a4f3bc151afe4521442c
Cederic Martinez <cedzkii81@gmail.com>
2019-09-04 18:16:17 +0800

    unang commit

---
+++ socket_checker.py
@@ -0,0 +1,29 @@
+import socket
+import time
+import logging
+
+
+#sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def check_port_status():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex(('127.0.0.1',80))
+    if result == 0:
+        return "Port is open"
+    else:
+        return "Port is not open"
+    sock.close()
+counter = 0
+while True:
+    #print('\x1b[2J')
+    port_stat = check_port_status()
+    counter = counter + 1
+    time.sleep(1)
+    #print(counter)
+
+    logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+    try:
+        logging.warning('This will get logged to a file' + str(counter) + " " + port_stat)
+    except:
+        logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+        logging.warning('This will get logged to a file' + str(counter) + " " + port_stat)
